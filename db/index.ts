b1541a27d8cbe54a9b909e5bc44710407ea7e3b0
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-12-30 14:59:22 -0600

    Also add dialect options to index

--- db/index.ts
+++ db/index.ts
@@ -15,6 +15,12 @@ const envConfig = {
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 } as any
 
