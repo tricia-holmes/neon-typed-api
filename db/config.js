44354de00369a669344e4255b28fb03ad9bd7709
Tricia Holmes <tricia.michele.holmes@gmail.com>
2022-12-30 14:49:40 -0600

    Update config again

--- db/config.js
+++ db/config.js
@@ -1,2 +1,16 @@
-// Keeping this here so sequelize cli can function correctly
-module.exports = {}
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  },
+  test: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  },
+}
